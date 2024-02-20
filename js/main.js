608d9c17068d96ecb179f7b9581aeaa66ed98bb9
gangstapetergriffin <144125776+gangstapetergriffin@users.noreply.github.com>
2024-02-20 12:31:09 +0100

    :D

--- js/main.js
+++ js/main.js
@@ -1,38 +1,67 @@
-document.getElementById("title").innerText = ":3";
 //game window reference
 const gameWindow = document.getElementById("gameWindow");
 
 //inventory
+let inventory = [];
 inventoryList = document.getElementById("inventoryBox")
 
 //main character
 const mainCharacter = document.getElementById("Hero");
 const offsetCharacter = 16;
 
-const tree1 = document.getElementById("squareTree")
-
 gameWindow.onclick = function (e) {
     var rect = gameWindow.getBoundingClientRect();
     var x = e.clientX - rect.left;
     var y = e.clientY - rect.top;
 
-    //FIX: character going out of bounds and character not animating on the first click
     //TODO: calc offset base on character size
-    mainCharacter.style.left = x - offsetCharacter + "px"
-    mainCharacter.style.top = y - offsetCharacter + "px"
+    if (e.target.id !== "heroImg") {
+        mainCharacter.style.left = x - offsetCharacter + "px"
+        mainCharacter.style.top = y - offsetCharacter + "px"
+    }
 
     switch (e.target.id) {
-        case "squareTree":
-            tree1.style.opacity = 0.5;
         case "key":
-            console.log("YOU HAVE FOUND THE KEY");
-            document.getElementById("key").remove();
-            const keyElement = document.createElement("li");
-            keyElement.id = "inv-key";
-            keyElement.innerText = "key"
-            inventoryList.appendChild(keyElement);
+            getItem("Rusty Key", "rustykey")
             break
-        default:
-            tree1.style.opacity = 1;
+        case "well":
+            getItem("coin", "coin")
+            break
+        case "doorWizardHut":
+            if (checkItem("Rusty Key")) {
+                console.log("dor OPEN YESSS");
+            } else if (checkItem("coin")) {
+                console.log("my coin GONE WAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH.")
+                removeItem("coin", "coin");
+            } else {
+                console.log(" I NED KEY FOR DOOR ")
+            }
+            break
     }
+}
+
+function getItem(itemName, itemId) {
+    if (!checkItem(itemName)) {
+        inventory.push(itemName);
+        showItem(itemName, itemId);
+    }
+}
+
+function checkItem(itemName) {
+    return inventory.includes(itemName);
+}
+
+function showItem(itemName, itemId) {
+    console.log("YOU HAVE FOUND " + itemName + "!");
+    const keyElement = document.createElement("li");
+    keyElement.id = itemId;
+    keyElement.innerText = itemName
+    inventoryList.appendChild(keyElement);
+}
+
+function removeItem(itemName, itemId) {
+    inventory = inventory.filter(function (newInventory) {
+        return newInventory !== itemName
+    });
+    document.getElementById(itemId).remove();
 }
